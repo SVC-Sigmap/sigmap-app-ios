795fc1d5fbde26e4c0a9cba79650765c50627484
Jack Puschnigg <jack.puschnigg@gmail.com>
2023-01-23 19:28:25 -0500

    Initial Commit

---
+++ sigmap-ios/sigmap_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  sigmap_iosApp.swift
+//  sigmap-ios
+//
+//  Created by Jack Puschnigg on 2023-01-23.
+//
+
+import SwiftUI
+
+@main
+struct sigmap_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
