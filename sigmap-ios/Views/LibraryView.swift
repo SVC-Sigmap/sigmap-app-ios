bd84d4103d660c90d3c330616c11cfec3896563a
jacobvisalli <jacobvisalli@gmail.com>
2023-02-23 22:01:50 -0500

    map preview tile

--- sigmap-ios/Views/LibraryView.swift
+++ sigmap-ios/Views/LibraryView.swift
@@ -11,6 +11,15 @@ import SwiftUI
 struct LibraryView: View {
     @State var isEditing: Bool = true
     @State var alertIsVisible: Bool = false
+    @State var roomNames = ["room 1", "room 2", "room 3", "room 4", "room 5", "room 6", "room 7"]
+    
+    private var columns = Array(repeating: GridItem(.flexible(), spacing: -100), count: 2)
+    
+    init() {
+            UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+
+            UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.white]
+        }
 
     var body: some View {
         NavigationView {
@@ -19,7 +28,6 @@ struct LibraryView: View {
                 Color(UIColor(red: 30/255, green: 30/255, blue: 30/255, alpha: 1))
                     .ignoresSafeArea()
                 
-                // placeholder text
                 Text("WiFi Maps")
                     .font(.custom("Helvetica Neue", size: 26))
                     .foregroundColor(.white)
@@ -32,17 +40,31 @@ struct LibraryView: View {
                         .font(.custom("Helvetica Neue", size: 26))
                         .frame(width:350, height: 725, alignment: .topTrailing)
                 }
-                
+                VStack (spacing: 10) {
+                    ScrollView(){
+                        LazyVGrid(columns: columns) {
+                            ForEach(roomNames, id: \.self) { roomName in
+                                HStack{
+                                    VStack{
+                                        NavigationLink(destination: MapView(roomName: roomName)){
+                                            Image(systemName: "square.fill")
+                                                .font(.system(size: 150))
+                                                .foregroundColor(.white)
+                                        }
+                                        Text(roomName)
+                                            .foregroundColor(Color.white)
+                                    }
+                                }
+                            }
+                            
+                        }
+                    }.padding(.top, 100).padding(.bottom, 100)
+                }
+                    
                 Divider()
                     .frame(height: 1)
                     .overlay(Color.white)
                     .frame(width: 500, height: 630, alignment: .top)
-
-                Button("Button") {
-                    self.alertIsVisible = true
-                }
-                    .opacity(isEditing ? 0 : 1 )
-                    .frame(width:350, height: 725, alignment: .center)
                 
                 Divider()
                     .overlay(.white)
