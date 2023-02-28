b0278e1dbd28739e5ea18853d9ee9cdb3ec86b78
Jack Puschnigg <jack.puschnigg@gmail.com>
2023-02-28 14:45:13 -0500

    Added login, signout, API req, etc.

---
+++ sigmap-ios/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  sigmap-ios
+//
+//  Created by Jack Puschnigg on 2023-02-28.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
